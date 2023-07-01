59f27b19395041ab64df813092dba287ff8f9d23
msgsn <richmilk156@gmail.com>
2023-07-02 05:54:34 +0900

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://lateral-canoe-273.notion.site/TORI-93dd12b37bc943b2a1238806e5858c8f',
         pages: [
             // {
             //     page_id: '<page_id>',
